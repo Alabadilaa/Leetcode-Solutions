4cf46a63f8a572a38656310f266671de43a19b28
Alabadilaa <alabadila.malai1@gmail.com>
2024-06-26 15:19:30 +0100

    Solved the Minimum Number Of Moves To Seat Everyone Problem, beats 89.50% Runtime and 88.11% Memory of users with Java

---
+++ Greedy/MinMovesToSeatEveryone.java
@@ -0,0 +1,16 @@
+package Greedy;
+
+import java.util.Arrays;
+
+public class MinMovesToSeatEveryone {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        int moves = 0;
+        for (int i = 0; i < seats.length; i++) {
+            moves += Math.abs(seats[i] - students[i]);
+        }
+        return moves;
+    }
+}
+
