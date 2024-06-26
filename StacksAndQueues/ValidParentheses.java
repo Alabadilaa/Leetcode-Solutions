63dbe384316b63c9617f18810787d353165d3678
Alabadilaa <alabadila.malai1@gmail.com>
2024-06-26 12:57:11 +0100

    Solved the Top K Frequent Elements Problem, beats 91.07% Runtime and 43.88% Memory of users with Java

---
+++ StacksAndQueues/ValidParentheses.java
@@ -0,0 +1,45 @@
+package StacksAndQueues;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        char[] open = {'(', '{', '['};
+        char[] close = {')', '}', ']'};
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            boolean isOpen = false;
+
+            for (char value : open) {
+                if (c == value) {
+                    stack.push(c);
+                    isOpen = true;
+                    break;
+                }
+            }
+
+            if (!isOpen) {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+                boolean matchFound = false;
+
+                for (int j = 0; j < close.length; j++) {
+                    if (c == close[j] && top == open[j]) {
+                        matchFound = true;
+                        break;
+                    }
+                }
+
+                if (!matchFound) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
