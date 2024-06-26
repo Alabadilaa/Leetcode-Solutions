87e12558ec7a632d2122b14b673a4cc7af826456
Alabadilaa <alabadila.malai1@gmail.com>
2024-06-26 12:20:53 +0100

    Solved the Merge Sorted Array Problem, beats 33.53% Runtime and 32.08% Memory

---
+++ Hashing/MergeSortedArray.java
@@ -0,0 +1,14 @@
+package Hashing;
+
+import java.util.Arrays;
+
+public class MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = 0, j = m; i < n; i++) {
+            nums1[j] = nums2[i];
+            j++;
+        }
+
+        Arrays.sort(nums1);
+    }
+}
