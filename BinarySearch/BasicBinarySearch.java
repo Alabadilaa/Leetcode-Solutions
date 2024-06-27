641dcf707c57ad3130f88717388c201134268cc9
Alabadilaa <alabadila.malai1@gmail.com>
2024-06-27 16:26:58 +0100

    Solved the Binary Search Problem, Beats 100.00% Runtime and 66.58% Memory of users with Java

---
+++ BinarySearch/BasicBinarySearch.java
@@ -0,0 +1,22 @@
+package BinarySearch;
+
+public class BasicBinarySearch {
+    public int search(int[] nums, int target) {
+    
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right){
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
