087b5dfcee0151796ca221fd5dfc9acdb689a2d8
Alabadilaa <alabadila.malai1@gmail.com>
2024-06-27 15:33:44 +0100

    Solved the Find Center Of Star Graph Problem, Beats 100.00% Runtime and 88.07% Memory of users with Java

---
+++ TreesAndGraphs/CenterOfStarGraph.java
@@ -0,0 +1,10 @@
+package TreesAndGraphs;
+
+public class CenterOfStarGraph {
+
+        public int findCenter(int[][] edges) {
+            return (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]) ? edges[0][0] : edges[0][1];
+    
+        }
+    }
+
