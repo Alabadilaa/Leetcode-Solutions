dfe7fb52c6e47c3170d2125def0efebd0d57808b
Alabadilaa <alabadila.malai1@gmail.com>
2024-07-03 12:19:02 +0100

    Solved the Same Tree Problem, beats 100.00% Runtime and 7.92% Memory of users with Java

--- TreesAndGraphs/SameTree.java
+++ TreesAndGraphs/SameTree.java
@@ -16,7 +16,7 @@ public class SameTree {
  *     }
  * }
  */
-class Solution {
+
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null) {
             return true;
@@ -30,4 +30,4 @@ class Solution {
         return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
-}
+
