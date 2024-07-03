67409aca8c172f4a0f29ef729e98ccfcb122a1fd
Alabadilaa <alabadila.malai1@gmail.com>
2024-07-03 12:20:35 +0100

    Solved the Maximum Depth Of Binary Tree Problem, beats 100.00% Runtime and 95.25% Memory of users with Java

---
+++ TreesAndGraphs/MaxDepthOfBinaryTree.java
@@ -0,0 +1,30 @@
+package TreesAndGraphs;
+
+public class MaxDepthOfBinaryTree {
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if ((root.left == null) && (root.right == null)) {
+            return 1;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
+
